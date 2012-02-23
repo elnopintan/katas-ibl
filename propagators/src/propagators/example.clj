b45596242614c7b89a8469b5c05c49ec71b37d6e
Ignacio Blasco <elnopintan@gmail.com>
2012-02-23 01:44:02 +0100

    another example

--- propagators/src/propagators/example.clj
+++ propagators/src/propagators/example.clj
@@ -4,14 +4,15 @@
   )
 
 (def plantas  ["planta1" "planta2" "planta3"])
-(println "Creando sensores de temperatura")
+
+(comment (println "Creando sensores de temperatura")
 (doseq  [s ["s1" "s2" "s3"] p plantas]
   (make-cell "sensor" s p "Oeste 1"))
 
 (println "Creando sensor de puerta")
-(make-cell "puerta" "Oeste 1")
+(make-cell "puerta" "Oeste 1") )
 
-(defn destroy-example [] (map #(destroy-cell %) (get-cell ["Oeste 1"])))
+(defn destroy-example [building] (map #(destroy-cell %) (get-cell [building])))
 (defn avg [& vals] (float (/ (apply + vals) (count vals))))
 
 (defrecord Person [person-name in-out])
@@ -21,18 +22,39 @@
 		     (conj people person-name)
 		     (disj people person-name))))
 
-(defn create-temp-propagators []
+(defn create-temp-propagators [building]
   (doseq [ p plantas]
-    (create-propagator [["sensor" p]] (only-input avg) ["temp-planta" p "Oeste 1"]))
-  (create-propagator [["temp-planta" "Oeste 1"]] (only-input avg) ["temp-edificio" "Oeste 1"]))
+    (create-propagator [["sensor" p]] (only-input avg) ["temp-planta" p building]))
+  (create-propagator [["temp-planta" building]] (only-input avg) ["temp-edificio" building]))
 
-(defn create-people-propagator []
-  (create-propagator [["puerta"]] get-people ["people" "Oeste 1"]))
+(defn create-people-propagator [building]
+  (create-propagator [["puerta"]] get-people ["people" building]))
 
-(defn create-building-popagators []
-  (create-propagator [["people"]] #(assoc %1 :people (count %2)) ["Oeste 1" "summary"])
-  (create-propagator [["temp-edificio"]] #(assoc %1 :temperature %2) ["Oeste 1" "summary"]))
+(defn create-building-popagators [building]
+  (create-propagator [["people"]] #(assoc %1 :people (count %2)) [building "summary"])
+  (create-propagator [["temp-edificio"]] #(assoc %1 :temperature %2) [building  "summary"]))
 
-(defn all-temps [n] (map #(new-data % n) (get-cell ["sensor"])))
+(defn all-temps [n] (doseq [c (get-cell ["sensor"])] (new-data c n)))
 (defn in-building [p] (new-data (first (get-cell ["puerta"])) (Person. p "IN")))
 (defn out-building [p] (new-data (first (get-cell ["puerta"])) (Person. p "OUT")))
+
+(defn create-example [sn pn bn]
+  (destroy-all-cells)
+  (reset-props)
+  (letfn [
+          (sensor [s p b] (str "s" s p b))
+          (plant [p] (str "p" p))
+          (building [b] (str "b" b))]
+    (doseq [s (range sn) p (range pn) b (range bn)]
+;      (print "creating cell " (sensor s p b))
+      (make-cell "sensor" (sensor s p b) (plant p) (building b)))
+    (doseq [p (range pn) b (range bn)]
+      (println " creating prop for plant " (plant p) (building b)) 
+            (create-propagator [["sensor" (plant p) (building b)]] (only-input +) ["plant" (plant p) (building b)]))
+    (doseq [b (range bn)]
+      (println " creating prop for building " (building b))
+      (create-propagator [["plant" (building b)]] (only-input +) ["building" (building b)]))
+;    (println " creating prop for city")
+    (create-propagator [["building"]] (only-input +) ["city"])))
+
+ 
--- propagators/src/propagators/prop_api.clj
+++ propagators/src/propagators/prop_api.clj
@@ -1,9 +1,13 @@
 (ns propagators.prop-api
   (use propagators.cells)
   (require [clojure.set :as sets] ))
+
 (defonce props (atom {}))
-(declare new-data)
+
+(declare new-data run-propagator)
+
 (defrecord propagator [prop-agent tags-in f cell-out-tags])
+
 (defn create-propagator [tags-in f cell-out]
   (let [new-prop (propagator. (agent nil) tags-in f cell-out)]
     (when (empty? (get-cell cell-out)) (apply make-cell cell-out)) 
@@ -14,7 +18,7 @@
     (run-propagator nil new-prop)))
 
 (defn run-propagator [ _ {:keys [tags-in f cell-out-tags]}]
-  (let [cells (apply get-cell tags-in)
+  (let [cells (apply get-cell-values tags-in)
         values (map #(:value %) cells)
         {value-out :value :as cell-out} (first (get-cell cell-out-tags))
         value-out (if (= value-out "nothing") nil value-out)]
@@ -36,3 +40,6 @@
   (notify-propagators (:tags cell)))
 
 (defn only-input [f] (fn [ _ & cells] (apply f cells)))
+
+(defn reset-props []
+  (reset! props {}))
